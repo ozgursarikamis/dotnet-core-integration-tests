2385586f4fe83444d35f806438db27548f4f27f5
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-29 02:19:20 +0300

    HealthCheckTests class

---
+++ TestBookings.Merchandise.Api.IntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using TennisBookings.Merchandise.Api;
+using Xunit;
+
+namespace TestBookings.Merchandise.Api.IntegrationTests
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+    }
+}
