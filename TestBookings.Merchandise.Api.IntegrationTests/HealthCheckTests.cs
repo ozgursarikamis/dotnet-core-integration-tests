7fca5b58e2b7d9b916fa1413090d71e029a1b8d8
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-29 02:23:07 +0300

    first endpoint test

--- TestBookings.Merchandise.Api.IntegrationTests/HealthCheckTests.cs
+++ TestBookings.Merchandise.Api.IntegrationTests/HealthCheckTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using TennisBookings.Merchandise.Api;
 using Xunit;
@@ -7,11 +9,19 @@ namespace TestBookings.Merchandise.Api.IntegrationTests
 {
     public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
     {
-        public HttpClient Factory { get; }
+        public HttpClient HttpClient { get; }
 
         public HealthCheckTests(WebApplicationFactory<Startup> factory)
         {
-            Factory = factory.CreateDefaultClient();
+            HttpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task HealthCheck_ReturnsOk()
+        {
+            var response = await HttpClient.GetAsync("/healthcheck");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
         }
     }
 }
