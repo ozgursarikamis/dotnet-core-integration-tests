2eab89d0c19912b6bc9672da4946506045d1a138
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-29 04:54:31 +0300

    Faking dependencies, defining test boundries, replacing services in the Test Client

--- TestBookings.Merchandise.Api.IntegrationTests/Controllers/StockControllerTests.cs
+++ TestBookings.Merchandise.Api.IntegrationTests/Controllers/StockControllerTests.cs
@@ -3,7 +3,12 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using TennisBookings.Merchandise.Api;
+using TennisBookings.Merchandise.Api.Data.Dto;
+using TennisBookings.Merchandise.Api.External.Database;
+using TestBookings.Merchandise.Api.IntegrationTests.Fakes;
 using TestBookings.Merchandise.Api.IntegrationTests.Models;
 using Xunit;
 
@@ -12,11 +17,15 @@ namespace TestBookings.Merchandise.Api.IntegrationTests.Controllers
     public class StockControllerTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         public HttpClient Client { get; }
+        public WebApplicationFactory<Startup> Factory;
 
         public StockControllerTests(WebApplicationFactory<Startup> factory)
         {
             var baseAddress = new Uri("http://localhost/api/stock/");
-            Client = factory.CreateDefaultClient(baseAddress);
+            factory.ClientOptions.BaseAddress = baseAddress;
+
+            Client = factory.CreateClient();
+            Factory = factory;
         }
         
         [Fact]
@@ -26,12 +35,6 @@ namespace TestBookings.Merchandise.Api.IntegrationTests.Controllers
             response.EnsureSuccessStatusCode();
         }
 
-        [Fact]
-        public async Task GetStockTotal_ReturnsExpectedJsonContentString()
-        {
-            var response = await Client.GetStringAsync("total");
-            Assert.Equal("{\"stockItemTotal\":100}", response);
-        }
 
         [Fact]
         public async Task GetStockTotal_ReturnsExpectedJsonContentType()
@@ -48,6 +51,30 @@ namespace TestBookings.Merchandise.Api.IntegrationTests.Controllers
 
             Assert.NotNull(model);
             Assert.True(model.StockItemTotal > 0);
+        }
+
+        [Fact]
+        public async Task GetStockTotal_ReturnsExpectedStockQuantity()
+        {
+            var cloudDatabase = new FakeCloudDatabase(new []
+            {
+                new ProductDto{ StockCount = 200}, 
+                new ProductDto{ StockCount = 300}, 
+                new ProductDto{ StockCount = 500}, 
+            });
+
+            // Registering a service:
+            var client = Factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<ICloudDatabase>(cloudDatabase);
+                });
+            }).CreateClient();
+
+            var model = await client.GetFromJsonAsync<ExpectedStockTotalOutputModel>("total");
+            
+            Assert.Equal(1000, model.StockItemTotal);
         }
     }
 }
--- src/TennisBookings.Merchandise.Api/Controllers/StockController.cs
+++ src/TennisBookings.Merchandise.Api/Controllers/StockController.cs
@@ -1,17 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TennisBookings.Merchandise.Api.Data;
 using TennisBookings.Merchandise.Api.Models.Output;
+using TennisBookings.Merchandise.Api.Stock;
 
 namespace TennisBookings.Merchandise.Api.Controllers
 {
     [ApiController, Route("api/[controller]")]
     public class StockController : ControllerBase
     {
-        [HttpGet("total")]
-        public IActionResult GetStockTotal()
+        public IProductDataRepository ProductRepository { get; }
+        public IStockCalculator StockCalculator { get; }
+
+        public StockController(
+            IProductDataRepository productRepository,
+            IStockCalculator stockCalculator
+        )
         {
-            //return Ok("{\"stockItemTotal:\":100}"); // Generates text/plain, not application/json
-            //return new JsonResult("{\"stockItemTotal\":100}");
-            return Ok(new StockTotalOutputModel {StockItemTotal = 100});
+            ProductRepository = productRepository;
+            StockCalculator = stockCalculator;
+        }
+
+        [HttpGet("total")]
+        public async Task<ActionResult<StockTotalOutputModel>> GetStockTotal()
+        {
+            var products = await ProductRepository.GetProductsAsync();
+
+            var totalStockCount = StockCalculator.CalculateStockTotal(products);
+
+            return Ok(new StockTotalOutputModel {StockItemTotal = totalStockCount });
         } 
     }
 }
