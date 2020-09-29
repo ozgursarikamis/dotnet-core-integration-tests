afc7df38110211b150f7fc627503f87aea31cbf4
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-30 01:59:03 +0300

    custom web application factory

---
+++ TestBookings.Merchandise.Api.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using TennisBookings.Merchandise.Api.External.Database;
+using TestBookings.Merchandise.Api.IntegrationTests.Fakes;
+
+namespace TestBookings.Merchandise.Api.IntegrationTests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup: class
+    {
+        public FakeCloudDatabase FakeCloudDatabase { get; set; }
+
+        public CustomWebApplicationFactory()
+        {
+            FakeCloudDatabase = FakeCloudDatabase.WithDefaultProducts();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddSingleton<ICloudDatabase>(FakeCloudDatabase);
+            });
+        }
+    }
+}
