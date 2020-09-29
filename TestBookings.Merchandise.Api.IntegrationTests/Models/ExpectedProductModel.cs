1700e8d3dd4e8cf65833cd818e4ee418c5e10fa9
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-30 01:58:52 +0300

    ProductsControllerTests

---
+++ TestBookings.Merchandise.Api.IntegrationTests/Models/ExpectedProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestBookings.Merchandise.Api.IntegrationTests.Models
+{
+    public class ExpectedProductModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Remaining { get; set; }
+        public IReadOnlyCollection<int> Ratings { get; set; }
+    }
+}
