a8a99e11b6c29719fde571ce767a430208732cc4
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-29 03:00:16 +0300

    test a controller

---
+++ TestBookings.Merchandise.Api.IntegrationTests/Controllers/CategoriesControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TennisBookings.Merchandise.Api;
+using Xunit;
+
+namespace TestBookings.Merchandise.Api.IntegrationTests.Controllers
+{
+    // test a controller:
+
+    public class CategoriesControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        public HttpClient HttpClient { get; }
+
+        public CategoriesControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            HttpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsSuccessStatusCode()
+        {
+            var response = await HttpClient.GetAsync("/api/categories");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
