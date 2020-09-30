4a522be58bda253f6dcae68b447acdf8cd22a265
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-30 05:09:34 +0300

    UI tests with AngleSharp

--- test/TennisBookings.Web.IntegrationTests/Pages/HomePageTests.cs
+++ test/TennisBookings.Web.IntegrationTests/Pages/HomePageTests.cs
@@ -1,15 +1,31 @@
 ﻿using System.Threading.Tasks;
+using TennisBookings.Web.IntegrationTests.Helpers;
 using Xunit;
 
 namespace TennisBookings.Web.IntegrationTests.Pages
 {
     public class HomePageTests : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
-        private readonly CustomWebApplicationFactory<Startup> _factory;
+        private CustomWebApplicationFactory<Startup> Factory { get; }
 
         public HomePageTests(CustomWebApplicationFactory<Startup> factory)
         {
-            _factory = factory;
-        }        
+            Factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_ReturnsPageWithExpected()
+        {
+            var client = Factory.CreateClient();
+            var response = await client.GetAsync("/");
+
+            response.EnsureSuccessStatusCode();
+
+            using var content = await HtmlHelpers.GetDocumentAsync(response);
+
+            var h1 = content.QuerySelector("h1");
+
+            Assert.Equal("Welcome to Tennis by the Sea!", h1.TextContent);
+        }
     }
 }
