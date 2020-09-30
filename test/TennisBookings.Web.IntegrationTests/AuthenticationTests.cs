a13f57550124acbe01fa70944535a48dcbfaa613
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-30 04:50:35 +0300

    Web.IntegrationTests project added

---
+++ test/TennisBookings.Web.IntegrationTests/AuthenticationTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace TennisBookings.Web.IntegrationTests
+{
+    public class AuthenticationTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        public CustomWebApplicationFactory<Startup> Factory { get; }
+
+        public AuthenticationTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            factory.ClientOptions.AllowAutoRedirect = false;
+            Factory = factory;
+        }
+    }
+}
