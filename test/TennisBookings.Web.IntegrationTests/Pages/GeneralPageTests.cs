a596727c48b4d76a1b933ab2dbe3b8997120549e
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-30 05:01:39 +0300

    testing urls

--- test/TennisBookings.Web.IntegrationTests/Pages/GeneralPageTests.cs
+++ test/TennisBookings.Web.IntegrationTests/Pages/GeneralPageTests.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Net.Http.Headers;
 using Xunit;
 
 namespace TennisBookings.Web.IntegrationTests.Pages
 {
     public class GeneralPageTests : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
-        private readonly CustomWebApplicationFactory<Startup> _factory;
+        public CustomWebApplicationFactory<Startup> Factory { get; }
 
         public GeneralPageTests(CustomWebApplicationFactory<Startup> factory)
         {
-            _factory = factory;
+            Factory = factory;
+        }
+
+        public static IEnumerable<object[]> ValidUrls => new List<object[]>
+        {
+            new object[] {"/"}, new object[] {"/Index"}, new object[] {"/About"}, new object[] {"/Enquiry"}
+        };
+
+        [Theory, MemberData(nameof(ValidUrls))]
+        public async Task ValidUrls_ReturnSuccessAndExpectedContentType(string path)
+        {
+            var expected = new MediaTypeHeaderValue("text/html");
+
+            var client = Factory.CreateClient();
+
+            var response = await client.GetAsync(path);
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal(expected.MediaType, response.Content.Headers.ContentType.MediaType);
         }
     }
 }
