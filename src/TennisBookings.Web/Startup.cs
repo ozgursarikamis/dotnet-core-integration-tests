7b16ef93a86c32223bc5cc690d0a74598ab3ce28
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-30 05:22:40 +0300

    Testing dynamic page content, mimicking Configuration

--- src/TennisBookings.Web/Startup.cs
+++ src/TennisBookings.Web/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
--- test/TennisBookings.Web.IntegrationTests/Pages/HomePageTests.cs
+++ test/TennisBookings.Web.IntegrationTests/Pages/HomePageTests.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using Microsoft.Extensions.Configuration;
 using TennisBookings.Web.IntegrationTests.Helpers;
 using Xunit;
 
@@ -26,6 +30,42 @@ namespace TennisBookings.Web.IntegrationTests.Pages
             var h1 = content.QuerySelector("h1");
 
             Assert.Equal("Welcome to Tennis by the Sea!", h1.TextContent);
+        }
+
+        public static IEnumerable<object[]> ConfigVariations => new List<object[]>
+        {
+            // global, page, should show:
+            new object[] { false, false, false },
+            new object[] { true, false, false },
+            new object[] { false, true, false },
+            new object[] { true, true, true },
+        };
+
+        [Theory]
+        [MemberData(nameof(ConfigVariations))]
+        public async Task HomePageIncludesForecast_ForExpactations(
+            bool globalEnabled, bool pageEnabled, bool shouldShow)
+        {
+            var client = Factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddInMemoryCollection(new Dictionary<string, string>
+                    {
+                        {"Features:WeatherForecasting:EnableWeatherForecast", globalEnabled.ToString()},
+                        {"Features:HomePage:EnableWeatherForecast", pageEnabled.ToString()},
+                    });
+                });
+            }).CreateClient();
+
+            var response = await client.GetAsync("/");
+
+            using var content = await HtmlHelpers.GetDocumentAsync(response);
+
+            var forecastDiv = content.All
+                .SingleOrDefault(e => e.Id == "weather-forecast" && e.LocalName == TagNames.Div);
+
+            Assert.Equal(shouldShow, forecastDiv != null);
         }
     }
 }
