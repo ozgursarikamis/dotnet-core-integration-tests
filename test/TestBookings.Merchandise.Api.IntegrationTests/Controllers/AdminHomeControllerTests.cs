3f18178f8a8098d7a6746c76cbbd5caffcccf9fa
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-30 23:30:17 +0300

    authorization test

---
+++ test/TestBookings.Merchandise.Api.IntegrationTests/Controllers/AdminHomeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using TennisBookings.Merchandise.Api;
+using Xunit;
+
+namespace TestBookings.Merchandise.Api.IntegrationTests.Controllers
+{
+    public class AdminHomeControllerTests: IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private CustomWebApplicationFactory<Startup> Factory { get; }
+
+        public AdminHomeControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            factory.ClientOptions.AllowAutoRedirect = false;
+            Factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_SecurePageIsForbiddenForAnUnauthenticatedUser()
+        {
+            var client = Factory.CreateClient();
+            var response = await client.GetAsync("/Admin");
+             
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.StartsWith("http://localhost/identity/account/login", 
+                response.Headers.Location.OriginalString, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
